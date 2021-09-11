d0feb3c8ac9a18e71f9925b98bf66a471b810279
alfinanur17 <alfinanur0117@gmail.com>
2021-09-11 22:26:54 +0700

    menambah

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\NewsController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
